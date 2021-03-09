1c5b05309e149775bc6e7b6fe976df55add06a6f
[abdhmd] <[sirhmd@outlook.com]>
2021-03-09 20:47:08 +0100

    image next edit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     images: {
       loader: 'cloudinary',
-      // path: 'cloudinary://774874966656811:4q-UDjejk_QY8Yn7DPUa0dypTow@dn3baky3u'
+      path:'cloudinary://524883465973621:YHiuWhqCLs6Hna1oNV_KqyF4n2o@dkyzngoz2'
+      // path: 'cloudinary://774874966656811:4q-UDjejk_QY8Yn7DPUa0dypTow@dn3baky3u' CLOUDINARY_URL=cloudinary://524883465973621:YHiuWhqCLs6Hna1oNV_KqyF4n2o@dkyzngoz2
       domains: ['res.cloudinary.com/']
     }
   };
